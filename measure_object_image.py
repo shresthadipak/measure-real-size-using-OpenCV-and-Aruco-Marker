dcef40458a4ef1bb17a2481736a8237f9f2ce8ac
Dipak <shresthadipak2049@gmail.com>
2023-01-17 14:55:43 +0545

    Feat: calculating the object size in pixel

--- measure_object_image.py
+++ measure_object_image.py
@@ -14,17 +14,23 @@ contours = detector.detect_object(img)
 
 for cnt in contours:
     # Draw polygon
-    cv2.polylines(img, [cnt], True, (0, 0, 255), 2)
+    # cv2.polylines(img, [cnt], True, (0, 0, 255), 2)
 
     # draw rect
     rect = cv2.minAreaRect(cnt)
     (x,y), (w, h), angle =  rect
 
-    cv2.circle(img, (int(x), int(y)), 5, (255, 0, 0), -1)
-
     box = np.int0(cv2.boxPoints(rect))
 
-    print(box)
+    # display the center point in an object
+    cv2.circle(img, (int(x), int(y)), 5, (255, 0, 0), -1)
+
+    # draw a rectangle box in objects
+    cv2.polylines(img, [box], True, (0, 255, 0), 2)
+
+    # display an image size 
+    cv2.putText(img, f'Width: {round(w, 1)}', (int(x), int(y)-50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+    cv2.putText(img, f'Height: {round(h, 1)}', (int(x), int(y)-30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
 
 cv2.imshow("Measure Object Size", img)
 
